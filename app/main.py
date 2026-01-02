0336fdcdb992b64fcd778692650ced6a9bd171fb
Suganth <suganthajish@gmail.com>
2026-01-02 23:37:22 +0530

    prediction API dev done

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from model.schema import TextRequest
+import joblib
 
-app = FastAPI(title="")
+app = FastAPI(title="AI Sentiment Classifier API")
+
+model = joblib.load("./model/sentiment_model.pkl")
 
 @app.get("/")
 def index():
-    return "Hello"
+    return "Hello, this id AI text classifier"
+
+@app.post("/predict")
+def predict_sentiment(request:TextRequest):
+    prediction = model.predict([request.text])[0]
+    return{
+        "text":request.text,
+        "sentiment": prediction
+    }
